094de74f92d926f264e66165e77e17a73263872c
Hurm1nka <glebka.zotov.06@mail.ru>
2024-10-10 15:54:24 +0300

    slozhenie dvyx 4isel

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, resultAdd;
+            Console.WriteLine("vvedite pervoe 4islo");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("vvedite vtoroe 4islo");
+            b = Convert.ToInt32(Console.ReadLine());
+            resultAdd = a + b;
+            Console.WriteLine("symma =" + resultAdd);
+            Console.ReadKey();
+        }
+    }
+}
